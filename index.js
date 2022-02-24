aa6cb123f3f1665d4390a249a2ed0263d098e0ec
lukelmg <lukegutman14@gmail.com>
2022-02-24 17:11:07 -0500

    first

---
+++ index.js
@@ -0,0 +1,106 @@
+let resturants1 = ['!panda express', '!duck donuts', '!einstein bros. bagels', '!krispy kreme', '!auntie annes', '!peets coffee', '!starbucks', '!tim hortons', '!jamba juice', '!boston market', '!chick-fil-a', '!chickfila', '!kfc', '!bojangles', '!popeyes', '!zaxbys', '!baskin robbins', '!cookout', '!cook out', '!in-n-out', '!five guys', '!mcdonalds', '!shake shack', '!smashburger', '!sonic', '!wawa', '!wendys', '!whataburger', '!white castle', '!wayback burgers', '!del taco', '!taco bell', '!moes', '!salsaritas', '!dominos', '!pizza hut', '!cicis', '!fazolis', '!little caesars', '!mod pizza', '!papa johns', '!saladworks', '!arbys', '!chipotle', '!firehouse subs', '!quiznos', '!subway', '!panera bread', '!applebees', '!olive garden', '!ruby tuesday', '!golden corral', '!dennys', '!waffle house', '!buffalo wild wings', '!perkins', '!red robin', '!cheesecake factory', '!bonefish', '!red lobster', '!cracker barrel', '!longhorn', '!longhorn stakehouse', '!texas roadhouse', '!chuck e cheese', '!sheetz', '!olivegarden', '!redlobster', '!wendys', '!outback', '!chilis']; 
+
+let resturants = ["McDonald’s","Starbucks","Subway","Taco Bell","Chick-fil-A","Burger King","Wendy’s","Dunkin’","Domino’s","Panera Bread","Pizza Hut","Chipotle Mexican Grill","Sonic Drive-In","KFC","Applebee’s","Olive Garden","Arby’s","Little Caesars","Buffalo Wild Wings","Dairy Queen","Panda Express","Chili’s Grill &amp; Bar","Jack in the Box","IHOP","Popeyes Louisiana Kitchen","Texas Roadhouse","Papa John’s","Denny’s","Outback Steakhouse","Cracker Barrel","Whataburger","Red Lobster","Jimmy John’s Gourmet Sandwiches","The Cheesecake Factory","Hardee’s","Zaxby’s","LongHorn Steakhouse","Golden Corral","Five Guys Burgers and Fries","Culver’s","Red Robin Gourmet Burgers and Brews","Carl’s Jr.","Waffle House","Bojangles’","Wingstop","Raising Cane’s Chicken Fingers","TGI Fridays","Jersey Mike’s Subs","BJ’s Restaurant &amp; Brewhouse","Steak ‘n Shake","In-N-Out Burger","P.F. Chang’s","El Pollo Loco","Bob Evans","Qdoba Mexican Eats","Del Taco","Krispy Kreme","Hooters","Papa Murphy’s Pizza","Firehouse Subs","Tim Hortons","Ruth’s Chris Steak House","Church’s Chicken","Moe’s Southwest Grill","Cheddar’s Scratch Kitchen","Jason’s Deli","Ruby Tuesday","McAlister’s Deli","Carrabba’s Italian Grill","California Pizza Kitchen","Baskin-Robbins","Perkins Restaurant &amp; Bakery","Marco’s Pizza","Bonefish Grill","Yard House","Checkers Drive-In Restaurants","Auntie Anne’s","Boston Market","Captain D’s Seafood Kitchen","White Castle","Dave &amp; Buster’s","Noodles &amp; Co.","Logan’s Roadhouse","Einstein Bros. Bagels","Dutch Bros. Coffee","Jamba Juice","Freddy’s Frozen Custard &amp; Steakburgers","Tropical Smoothie Cafe","O’Charley’s","Shake Shack","Potbelly Sandwich Shop","The Capital Grille","Long John Silver’s","The Habit Burger Grill","Round Table Pizza","Mellow Mushroom","Smoothie King","Miller’s Ale House","Maggiano’s Little Italy","First Watch","Hungry Howie’s Pizza","Dickey’s Barbecue Pit","Charleys Philly Steaks","Chuy’s","MOD Pizza","Chuck E. Cheese’s","Portillo’s","Pollo Tropical","Krystal Company","Cold Stone Creamery","Zoes Kitchen","Taco John’s","Pappadeaux Seafood Kitchen","Corner Bakery Cafe","Famous Dave’s","Jet’s Pizza","Schlotzsky’s","Cicis","Saltgrass Steak House","On The Border Mexican Grill &amp; Cantina","Village Inn","Twin Peaks","Au Bon Pain","Texas de Brazil Churrascaria","Blaze Pizza","Taco Cabana","Bar Louie","Pei Wei Asian Diner","Ninety Nine Restaurants","Fleming’s Prime Steakhouse &amp; Wine Bar","Benihana","Fogo de Chao","Smashburger","Black Bear Diner","Rally’s Hamburgers","Caribou Coffee","Cooper’s Hawk Winery &amp; Restaurants","Peet’s Coffee &amp; Tea","Big Boy/Frisch’s Big Boy","Sarku Japan","Sizzler","Bill Miller Bar-B-Q","Old Chicago Pizza &amp; Taproom","Wienerschnitzel","Pizza Ranch","Seasons 52","Friendly’s","Braum’s Ice Cream &amp; Dairy Stores","Morton’s The Steakhouse","Huddle House","Legal Sea Foods","Pret a Manger","Godfather’s Pizza","Bahama Breeze Island Grille","Sbarro","Cafe Rio Mexican Grill","Rubio’s","Fazoli’s","Mastro’s Restaurants","Romano’s Macaroni Grill","Newk’s Eatery","Brio Tuscan Grille","A&amp;W All-American Food","Which Wich","Fuzzy’s Taco Shop","Uncle Julio’s","Torchy’s Tacos","Beef ‘O’ Brady’s","Hard Rock Cafe","Souplantation &amp; Sweet Tomatoes","Luby’s","Sonny’s BBQ","Uno Pizzeria &amp; Grill","Johnny Rockets","Penn Station East Coast Subs","Claim Jumper","Fuddruckers","Cinnabon","Buca di Beppo","Shoney’s","Bubba Gump Shrimp Co.","Del Frisco’s Double Eagle Steak House","Mimi’s Cafe","Houlihan’s","J. Alexander’s","Joe’s Crab Shack","McCormick &amp; Schmick’s","Firebirds Wood Fired Grill","Donatos Pizza","Lazy Dog Restaurant &amp; Bar","Old Country Buffet/HomeTown Buffet","Pappasito’s Cantina","The Melting Pot","Le Pain Quotidien","Golden Chick","Islands Fine Burgers &amp; Drinks","La Madeleine Country French Cafe","Wetzel’s Pretzels","Great Harvest Bread Co.","Smokey Bones Bar &amp; Fire Grill","Taco Bueno","The Original Pancake House","True Food Kitchen","Mountain Mike’s Pizza","Jack’s","Lucille’s Smokehouse Bar-B-Que","Eat’n Park","The Coffee Bean &amp; Tea Leaf","Kona Grill","LaRosa’s Pizzeria","Sweetgreen","Menchie’s Frozen Yogurt","Anthony’s Coal Fired Pizza","Bruegger’s Bagels","Farmer Boys","Roosters","Hurricane Grill &amp; Wings","Giordano’s","The Old Spaghetti Factory","Grand Lux Cafe","Wayback Burgers","Rainforest Cafe","PDQ","Pizza Pro","Pollo Campero","Duffy’s Sports Grill","Tijuana Flats","Black Angus Steakhouse","Quiznos","Shari’s Cafe and Pies","Biggby Coffee","Rosati’s Pizza","Bertucci’s","Yogurtland","BurgerFi","Lee’s Famous Recipe Chicken","Eddie V’s Prime Seafood","Great American Cookies","Bravo! Cucina Italiana","Baja Fresh Mexican Grill","Slim Chickens","The Brass Tap","WaBa Grill","STK","54th Street Restaurant &amp; Drafthouse","Metro Diner","Freebirds World Burrito","Togo’s Sandwiches","Costa Vida Fresh Mexican Grill","L&amp;L Hawaiian Barbecue"]
+
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+const keepAlive = require('./server')
+keepAlive()
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+const puppeteer = require('puppeteer');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+require('dotenv').config()
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+let responses = ['you idiot', 'why tho', 'plz stop', 'you fool', 'nincompoop', 'bruh', 'bruh stop it', 'no u', 'ur bad', 'imbecile', 'nitwit', 'wtf', 'what are you thinking', 'i cant handle that', 'baddd', 'blockhead', 'wtf do you want from me', 'why are you bugging me', 'you think im that smat?', 'you jerk', 'what the fuck', 'you stupid?']
+
+var schedule = require('node-schedule');
+
+let CROWN = [];
+let SOVI = [];
+
+var j = schedule.scheduleJob('*/5 * * * *', async function(){
+
+  // set up browser for puppeteer to look at phs website
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+    executablePath: '/usr/bin/chromium-browser'
+  });
+  
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0)
+  await page.goto('https://app.safespace.io/api/display/live-occupancy/7a9c0a24?view=percent', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  await delay(2000);
+
+  CROWN = await page.evaluate(() => {
+    let headingFromWeb = document.querySelectorAll("#occupancyPct");
+    const headingList = [...headingFromWeb];
+    return headingList.map(h => h.innerHTML);
+  });
+
+  await page.goto('https://app.safespace.io/api/display/live-occupancy/15da3cfa?view=percent', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  await delay(2000);
+
+  SOVI = await page.evaluate(() => {
+    let headingFromWeb = document.querySelectorAll("#occupancyPct");
+    const headingList = [...headingFromWeb];
+    return headingList.map(h => h.innerHTML);
+  });
+
+  var now = new Date();
+  var day = days[now.getDay()];
+
+  console.log(day);
+  console.log(CROWN[0].slice(0,-1));
+  console.log(SOVI[0].slice(0,-1));
+
+  await browser.close();
+});
+
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
+
+ let count = 0;
+
+
+
+ bot.on('message', msg => {
+    if (msg.content == '!crown' || msg.content == '!Crown') {
+      msg.channel.send('The occupancy of Crown Commons is approximately: ' + CROWN[0]);
+    }
+    if (msg.content == '!sovi' || msg.content == '!SoVi' || msg.content == '!Sovi') {
+      msg.channel.send('The occupancy of SoVi is approximately: ' + SOVI[0]);
+    }
+    if(msg.content == '!help') {
+      msg.channel.send('`!crown` get occupancy of Crown Dining Hall \n`!sovi` get occupancy of SoVi Dining Hall');
+    }
+    for (let i = 0; i < resturants.length; i++) {
+      if (msg.content.toUpperCase() === '!'+ resturants[i].toUpperCase()) {
+        msg.channel.send(responses[Math.floor(Math.random()*responses.length)]);
+      } else if (i < resturants1.length) {
+         if (msg.content.toUpperCase() === resturants1[i].toUpperCase()) {
+           msg.channel.send(responses[Math.floor(Math.random()*responses.length)]);
+        }
+      }
+    }
+});
